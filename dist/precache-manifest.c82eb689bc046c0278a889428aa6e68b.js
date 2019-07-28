eb0a19307b8be62ff8be12b022fb4a48bc67394c
Yash Pandey <yashpandey.science@gmail.com>
2019-07-28 23:41:02 +0530

    Deploying website after react build

---
+++ dist/precache-manifest.c82eb689bc046c0278a889428aa6e68b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b93cb906dc6cb7ee72bd69abaf9539e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2f7956f4d205ce57c7f",
+    "url": "/static/css/main.8965138d.chunk.css"
+  },
+  {
+    "revision": "fe581b5edb501095a996",
+    "url": "/static/js/2.91426ed4.chunk.js"
+  },
+  {
+    "revision": "b2f7956f4d205ce57c7f",
+    "url": "/static/js/main.aae993e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04def2f227242a4cf781f4140e16886a",
+    "url": "/static/media/atom.04def2f2.svg"
+  },
+  {
+    "revision": "4190ac1189db82bcbe4eddaa6a7908d7",
+    "url": "/static/media/checked.4190ac11.svg"
+  },
+  {
+    "revision": "0e4a2d6bd9c346d96aac3ace1ff2aba5",
+    "url": "/static/media/collaborate.0e4a2d6b.jpg"
+  },
+  {
+    "revision": "f2ee10e194f38e0e1dd3575ca760e5db",
+    "url": "/static/media/loading.f2ee10e1.svg"
+  }
+]);
